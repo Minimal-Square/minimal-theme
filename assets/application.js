5d0496472099f1be6e345ce02e5ced7e403648b1
Meiz <meizgraphic@gmail.com>
2021-04-14 16:12:11 +0200

    Video Commit

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,10 @@
 // Put your application javascript here
+
+$(function(){
+    $('#play-video').on('click', function(ev) {
+        $(this).fadeOut();
+        $("#video")[0].src += "&autoplay=1";
+        ev.preventDefault();
+    });
+});
+
